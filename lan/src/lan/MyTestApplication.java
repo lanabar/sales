3349ce51e20b1e2e36b4ddcdbd612a9a9f4164da
lanabar <lanabarikhan74@gmail.com>
2015-12-05 14:48:20 +0100

    first chang we did to try  in the code

--- lan/src/lan/MyTestApplication.java
+++ lan/src/lan/MyTestApplication.java
@@ -4,6 +4,7 @@ package lan;
 public class MyTestApplication {
 	public static void main(String [] args){
 		System.out.println("Hi lana");
+		System.out.println("first change in the code");
 	}
 	
 
